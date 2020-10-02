b206ae2c32a55dfc1cfc51737e559f42b6d0d8ea
NormandM <nmartin1956@gmail.com>
2020-10-02 15:49:26 -0400

    commit before upgrading

--- QuizVerbesItaliens/StatisticView/StatistiqueViewCell.swift
+++ QuizVerbesItaliens/StatisticView/StatistiqueViewCell.swift
@@ -12,15 +12,6 @@ class StatistiqueViewCell: UITableViewCell {
     @IBOutlet weak var labelForCell: UILabel!
     @IBOutlet weak var viewForCell: PieChartView!
     
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
+
 
 }
